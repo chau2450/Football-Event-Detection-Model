f28b662c727f294f32b9ae3004950ec1303fc9ce
chau2450 <chau2450@mylaurier.ca>
2023-09-03 13:41:08 -0400

    model architecture done

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from Football_Event_Detector.logging import logger
 
-logger.info("This is a test for the logging feature")
+# logger.info("This is a test for the logging feature")
 
+
+from Football_Event_Detector.YOLO import model
+
+obj = model.Yolo()
 
 
 
